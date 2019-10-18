722d1ef5221735690f706bd142779df00fe8e62b
sombrerosheep <sombrerosheep@gmail.com>
2019-10-17 21:02:02 -0500

    cleanup

--- src/rectf.c
+++ src/rectf.c
@@ -1,17 +1,6 @@
 #include <rectf.h>
 #include <SDL.h>
 
-SDL_Rect rectf_to_rect(const rectf *rect) {
-  SDL_Rect rec;
-
-  rec.x = rect->x;
-  rec.y = rect->y;
-  rec.w = rect->w;
-  rec.h = rect->h;
-
-  return rec;
-}
-
 vec2f rectf_center(const rectf *rect) {
   vec2f cen;
 

92211c012b67840beb1de0b892bba0a6b126210c
sombrerosheep <sombrerosheep@gmail.com>
2019-10-19 19:35:37 -0500

    add function to get rect intersection
    returns bool if there was an intersection. fills the intersect rect with data of the rect that makes up the intersection

--- src/rectf.c
+++ src/rectf.c
@@ -97,6 +97,51 @@ SDL_bool rectf_intersects_rectf(const rectf *a, const rectf *b) {
   return SDL_TRUE;
 }
 
+SDL_bool rectf_intersection(const rectf *a, const rectf *b, rectf *intersection) {
+  float a_min, a_max;
+  float b_min, b_max;
+
+  a_min = a->x;
+  a_max = a->x + a->w;
+  b_min = b->x;
+  b_max = b->x + b->w;
+
+  if (b_min > a_min) {
+    a_min = b_min;
+  }
+  if (b_max < a_max) {
+    a_max = b_max;
+  }
+
+  intersection->x = a_min;
+  intersection->w = a_max - a_min;
+
+  if (a_max <= a_min) {
+    return SDL_FALSE;
+  }
+
+  a_min = a->y;
+  a_max = a->y + a->h;
+  b_min = b->y;
+  b_max = b->y + b->h;
+
+  if (b_min > a_min) {
+    a_min = b_min;
+  }
+  if (b_max < a_max) {
+    a_max = b_max;
+  }
+
+  intersection->y = a_min;
+  intersection->h = a_max - a_min;
+
+  if (a_max <= a_min) {
+    return SDL_FALSE;
+  }
+
+  return SDL_TRUE;
+}
+
 int rectf_intersects_circle(const rectf *a, float x, float y, float radius) {
   vec2f ul, ur, ll, lr;
   float mag, max;
